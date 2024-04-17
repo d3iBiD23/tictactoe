99b09209238afa1c568850fdf636c0777d3223ae
NachoHerreroPuig <ignasi.herrero.espelt@elpuig.xeill.net>
2024-04-17 19:51:49 +0200

    Mostrar menu

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Hola");
+        System.out.println("TIC-TAC-TOE \n");
+        System.out.println("------------- \n");
+        System.out.println("1) Nova Partida");
+        System.out.println("2) Carregar Partida");
+        System.out.println("3) Configuració");
+        System.out.println("4) Sortir");
+
+        System.out.println("Escull una opció:");
+        int opció_seleccionada = sc.nextInt();
+
     }
 }
