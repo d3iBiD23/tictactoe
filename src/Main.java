a87ae0fec3961d4eb91ac3f85a783bc29fb05bca
d3iBiD23 <eddydavide.carrion.valarezo@elpuig.xeill.net>
2024-04-18 12:29:22 +0200

    Commit handling exception.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while (true){
+        boolean salir = false;
+
+        while (!salir) {
             System.out.println("TIC-TAC-TOE \n");
             System.out.println("------------- \n");
             System.out.println("1) Nova Partida");
@@ -12,23 +15,31 @@ public class Main {
             System.out.println("4) Sortir");
 
             System.out.println("Escull una opció: ");
-            int opció_seleccionada = sc.nextInt();
+            try {
+                int opcio_seleccionada = sc.nextInt();
 
-            switch(opció_seleccionada) {
-                case 1:
-                    System.out.println("Nova partida");
-                    break;
-                case 2:
-                    System.out.println("Carregar Partida");
-                    break;
-                case 3:
-                    System.out.println("Configuració");
-                    break;
-                case 4:
-                    System.out.println("Sortir");
-                    break;
-
+                switch (opcio_seleccionada) {
+                    case 1:
+                        System.out.println("Has escollit: Nova partida");
+                        break;
+                    case 2:
+                        System.out.println("Has escollit: Carregar Partida");
+                        break;
+                    case 3:
+                        System.out.println("Has escollit: Configuració");
+                        break;
+                    case 4:
+                        System.out.println("Has escollit: Sortir");
+                        salir = true; // Sortir del bucle
+                        break;
+                    default:
+                        System.out.println("Opció no vàlida. Torna a intentar.");
+                        break;
+                }
+            } catch (InputMismatchException ex) {
+                sc.nextLine();
             }
         }
+        sc.close();
     }
 }
