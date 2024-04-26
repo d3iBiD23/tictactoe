afcdcf0ee46408f65847598287329e42399e4964
NachoHerreroPuig <ignasi.herrero.espelt@elpuig.xeill.net>
2024-04-26 17:51:37 +0200

    afegir metodes novaPartida, jugadaGuanyadora, jugar amb not implemented excp

--- src/Joc.java
+++ src/Joc.java
@@ -1,3 +1,5 @@
+import jdk.jshell.spi.ExecutionControl;
+
 public class Joc {
     private char[][] taulell;
     private short torn;
@@ -8,5 +10,17 @@ public class Joc {
 
     public short getTorn() {
         return torn;
+    }
+
+    public void novaPartida() throws ExecutionControl.NotImplementedException{
+        throw new ExecutionControl.NotImplementedException("Mètode no creat");
+    }
+
+    public boolean jugadaGuanyadora(short fila, short columna) throws ExecutionControl.NotImplementedException{
+        throw new ExecutionControl.NotImplementedException("Mètode no creat");
+    }
+
+    public void jugar(short fila, short columna) throws ExecutionControl.NotImplementedException{
+        throw new ExecutionControl.NotImplementedException("Mètode no creat");
     }
 }
