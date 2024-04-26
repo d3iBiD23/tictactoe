04428447fa0774d0652cbb19cde9753af04dde7a
d3iBiD23 <eddydavide.carrion.valarezo@elpuig.xeill.net>
2024-04-26 17:45:53 +0200

    Creació de atributs i getters de la Classe Joc.

--- src/Joc.java
+++ src/Joc.java
@@ -1,3 +1,12 @@
 public class Joc {
+    private char[][] taulell;
+    private short torn;
 
+    public char[][] getTaulell() {
+        return taulell;
+    }
+
+    public short getTorn() {
+        return torn;
+    }
 }
