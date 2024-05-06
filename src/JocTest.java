f26c12b9df1abd658058dda6d2bf9f98423b8b3c
NachoHerreroPuig <ignasi.herrero.espelt@elpuig.xeill.net>
2024-05-06 17:44:31 +0200

    Implementació Test metode NOvaPartida()

--- src/JocTest.java
+++ src/JocTest.java
@@ -5,16 +5,23 @@ public class JocTest {
     @org.junit.jupiter.api.Test
     void testNovaPartida() {
         // Instanciar joc
-        Joc j = new Joc();
+        Joc joc = new Joc();
+
+        char[][] taulellNet = joc.getTaulell();
+        // Comprovar que el tablero se genera limpio (nadie ha jugado)
+        for (int i = 0; i < joc.getTaulell().length; i++){
+            for (int j = 0; j < joc.getTaulell().length; j++){
+                Assertions.assertEquals('·', taulellNet[i][j]);
+            }
+        }
 
         // Comprovar que es el turno 1
-        Assertions.assertEquals(1, j.getTorn());
-
-        // Comprovar que el tablero se genera limpio (nadie ha jugado)
+        Assertions.assertEquals(1, joc.getTorn());
     }
 
     @org.junit.jupiter.api.Test
     void testJugadaGuanyadora() {
+
     }
 
     @org.junit.jupiter.api.Test
