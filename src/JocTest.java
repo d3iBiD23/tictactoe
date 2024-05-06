8cab4bf10c47f15c88bdf083b576f6c92f44a46b
NachoHerreroPuig <ignasi.herrero.espelt@elpuig.xeill.net>
2024-05-06 17:04:00 +0200

    creació JocTest

---
+++ src/JocTest.java
@@ -0,0 +1,14 @@
+public class JocTest {
+
+    @org.junit.jupiter.api.Test
+    void testNovaPartida() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testJugadaGuanyadora() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void jugar() {
+    }
+}
