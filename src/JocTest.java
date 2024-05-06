a98aed3a7683558879f1ff75495d2c05e8c77880
d3iBiD23 <eddydavide.carrion.valarezo@elpuig.xeill.net>
2024-05-06 17:28:04 +0200

    Test novaPartida, comprovem que es el torn del jugador 1

--- src/JocTest.java
+++ src/JocTest.java
@@ -1,7 +1,16 @@
+import org.junit.jupiter.api.Assertions;
+
 public class JocTest {
 
     @org.junit.jupiter.api.Test
     void testNovaPartida() {
+        // Instanciar joc
+        Joc j = new Joc();
+
+        // Comprovar que es el turno 1
+        Assertions.assertEquals(1, j.getTorn());
+
+        // Comprovar que el tablero se genera limpio (nadie ha jugado)
     }
 
     @org.junit.jupiter.api.Test
@@ -10,5 +19,6 @@ public class JocTest {
 
     @org.junit.jupiter.api.Test
     void jugar() {
+
     }
 }
