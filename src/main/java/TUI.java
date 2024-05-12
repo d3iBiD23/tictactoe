8d8dbac9ae7526ca7ec87aca9ac8a3dec4bce573
NachoHerreroPuig <ignasi.herrero.espelt@elpuig.xeill.net>
2024-05-12 21:18:14 +0200

    Ampliació metode TUI per mostrar el taulell maquetat amb amb '|' '_' per delimitar les caselles

--- src/main/java/TUI.java
+++ src/main/java/TUI.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class TUI {
     private Scanner sc;
 
+    //mostra el menu d'opcions
     public short mostrarMenu() {
         sc = new Scanner(System.in);
 
@@ -26,9 +27,28 @@ public class TUI {
         }
     }
 
+    // mostra el taulell maequetat amb delimitadors de casella
+    public void mostrarTaulell(char[][] taulell, short torn) {
+        System.out.println("Torn del jugador " + (torn == 1 ? '1' : '2'));
 
-    public void mostrarTaulell(char[][] taulell, short turn) throws ExecutionControl.NotImplementedException {
-        throw new ExecutionControl.NotImplementedException("Mètode no creat");
+        for (int i = 0; i < taulell.length; i++) {
+            // Mostrar cada fila del tablero
+            for (int j = 0; j < taulell[i].length; j++) {
+                System.out.print(taulell[i][j]);
+
+                // Si no es la última columna, añadir una barra vertical
+                if (j < taulell[i].length - 1) {
+                    System.out.print(" | ");
+                }
+            }
+            // Nueva línea después de cada fila
+            System.out.println();
+
+            // Si no es la última fila, añadir una línea divisoria
+            if (i < taulell.length - 1) {
+                System.out.println("---------");
+            }
+        }
     }
     public short[][] recollirJugada() throws ExecutionControl.NotImplementedException {
         throw new ExecutionControl.NotImplementedException("Mètode no creat");
